8bc5261a5d8d845fed2d47cb13f31c80f90b240d
Trần Văn Huấn <tranvanhuan@Mac-mini-cua-Tran.local>
2025-11-17 16:22:27 +0700

    fix / UnAuth

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,15 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import { createRequire } from 'module';
+import fs from 'fs';
+import 'dotenv/config'; 
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require('../jiraapp-46b2d-firebase-adminsdk-fbsvc-92c070394e.json');
+const serviceAccountPath = process.env.GOOGLE_SERVICE_ACCOUNT_JSON;
+
+if (!serviceAccountPath) {
+  throw new Error('Environment variable GOOGLE_SERVICE_ACCOUNT_JSON not set!');
+}
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 initializeApp({
   credential: cert(serviceAccount),
